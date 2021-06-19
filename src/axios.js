47a799dca2f12eab952e1caa86ecc989060953ef
SaheenJasmine <63956055+developweb-Sah@users.noreply.github.com>
2021-06-20 01:19:29 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001",
+    // baseURL: "http://localhost:5001",
+    baseURL: "http://app-72d19b9f-f493-4977-85cf-f04d289f5132.cleverapps.io/",
 })
 
 export default instance;
